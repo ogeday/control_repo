1308ab6dbc5da7fb8baed6b5a7c71f7edc6a4b54
Ogeday Isiklar <ogeday@gmail.com>
2018-01-31 13:04:14 +0000

    hosts

--- site/profile/manifests/hosts.pp
+++ site/profile/manifests/hosts.pp
@@ -12,6 +12,11 @@ class profile::hosts {
     name => 'db.puppet.vm',
     ip => '172.18.0.2',
   }
+  host { 'minetest':
+    ensure => 'present',
+    name => 'minetest.puppet.vm',
+    ip => '172.18.0.4',
+  }
   host { 'master':
     ensure => 'present',
     name => 'master.puppet.vm',
