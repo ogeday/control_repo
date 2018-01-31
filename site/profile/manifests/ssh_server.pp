c15547a8c758efb9c2c1d5ff6b07e8771cb4afde
Ogeday Isiklar <ogeday@gmail.com>
2018-01-31 11:45:37 +0000

    rsa key

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
-	}  
+		key    => 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDCXR19RCVoBHYeLdS/iEGCRYKTojdFWCWI2YjmQb7i8kdEFk8SssHFp5s0FbAFeHMzo3jBvliXm4nsRivRwK+dU4p/qhxdAs3plb7gGxSiscvPcVaSp6anxKlzbdfEiROYxkFeQf46KpPNJ44yrIGogl3v4KVUYWbxjKHbJhhnT8BrgDW26KPAxTIsXWXnOBVpPGsRmj6pmx89sCx3vlnh16eDafisYznPAjtrCThFLELAIO1uP+MPVGQG65Tl2hHJK3lSgyECeoFUeS/WSHUX1vxD6eHeTvRPd59PfuogyWL6b4I9jjF5kK4hCjASLtTT/DSlfUS5M9q5pbRrQhMb root@master.puppet.vm',
+	}
 }
