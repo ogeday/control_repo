a6952c5f1d0f49d6c666e2188a29a9103e763e82
Ogeday Isiklar <ogeday@gmail.com>
2018-01-31 15:29:11 +0000

    ssh

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDCXR19RCVoBHYeLdS/iEGCRYKTojdFWCWI2YjmQb7i8kdEFk8SssHFp5s0FbAFeHMzo3jBvliXm4nsRivRwK+dU4p/qhxdAs3plb7gGxSiscvPcVaSp6anxKlzbdfEiROYxkFeQf46KpPNJ44yrIGogl3v4KVUYWbxjKHbJhhnT8BrgDW26KPAxTIsXWXnOBVpPGsRmj6pmx89sCx3vlnh16eDafisYznPAjtrCThFLELAIO1uP+MPVGQG65Tl2hHJK3lSgyECeoFUeS/WSHUX1vxD6eHeTvRPd59PfuogyWL6b4I9jjF5kK4hCjASLtTT/DSlfUS5M9q5pbRrQhMb',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDXP+24q8o123ZU5I5pFEOlCkxWdFgplA6Fc+7fL48r1LA6gvijSY8kQjMU5kqbG/Y9OTbCKXeQU7IvU3cGESUXhKmLG6IP9F0hP5zK7w21Gf+Sk8Pa4u5ns4P786uVulKxf2qfzpKUgQ4QxgAMWK3c7k2+ujReZQda2n5LZLXixOxl+yy/bN4yWnybTZj9zhB9gierHnRe0MkObFHWaEbVQLb/iiWzxUcJI9+kGVnfLFQHEdGEgtjKQpAfcnmiKC6j4aQMDqXZS4JU6Qyykgp5b5TfuFCKt9fz+4S2zNvMMCmCbldv6rosaPmTAWw3IoA6tfpYiJOABzk6UaCok8fX',
 	}
 }
